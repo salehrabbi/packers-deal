d770a525354840ad8b49879f95d663d419fc587e
salehrabbi <33930388+salehrabbi@users.noreply.github.com>
2018-03-04 19:23:51 +0600

    support section

--- js/custom.js
+++ js/custom.js
@@ -4,3 +4,14 @@ jQuery(document).ready(function ($) {
         time: 1000
     });
 });
+
+$('.patner').hover(function () {
+    $('.support').addClass('lightblue'); // add class when mouseover happen
+}, function () {
+    $('.support').removeClass('lightblue'); // remove class when mouseout happen
+});
+$('.operator').hover(function () {
+    $('.support').addClass('blue'); // add class when mouseover happen
+}, function () {
+    $('.support').removeClass('blue'); // remove class when mouseout happen
+});
