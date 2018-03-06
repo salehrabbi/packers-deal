d0b9852610f0850d691f06b1922a1e84c20b1d56
salehrabbi <33930388+salehrabbi@users.noreply.github.com>
2018-03-06 22:28:43 +0600

    off preloader

--- js/custom.js
+++ js/custom.js
@@ -52,13 +52,13 @@ $(document).scroll(function (e) {
 });
 
 
-$('body').addClass('preloader-active');
-
-$(window).on('load', function () {
-    $('.preloader').fadeOut();
-    $('.preloader-spinner').delay(350).fadeOut('slow');
-    $('body').removeClass('preloader-active');
-});
+//$('body').addClass('preloader-active');
+//
+//$(window).on('load', function () {
+//    $('.preloader').fadeOut();
+//    $('.preloader-spinner').delay(350).fadeOut('slow');
+//    $('body').removeClass('preloader-active');
+//});
 
 
 
